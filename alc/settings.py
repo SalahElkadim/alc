14ddf710d768d4d5f28b0417175b9c5944da5754
unknown <khaledgaber494@gmail.com>
2025-07-30 01:58:13 +0300

    first commit

--- alc/settings.py
+++ alc/settings.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,7 +152,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 FRONTEND_URL = 'http://localhost:3000'  # أو الـ URL بتاع الـ frontend
