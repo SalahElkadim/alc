cfd67effe89d17bb80ec6d2bd5afb8262e052171
unknown <khaledgaber494@gmail.com>
2025-08-07 20:46:33 +0300

    edit

--- alc/settings.py
+++ alc/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3#-m1uyln4jei7me&3=*+ued3w403@(72wxzg#$2@o_s@so_7l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
