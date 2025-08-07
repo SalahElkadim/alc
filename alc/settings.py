542e439f3802881db836123ce98f47b4216eff50
unknown <khaledgaber494@gmail.com>
2025-08-07 18:25:27 +0300

    edit

--- alc/settings.py
+++ alc/settings.py
@@ -113,12 +113,14 @@ WSGI_APPLICATION = 'alc.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
+    'default': dj_database_url.parse(
+        'postgresql://postgres:tHTpzmhrKmjpBODZffagOQKAVwzBYLBE@hopper.proxy.rlwy.net:17588/railway', 
+         conn_max_age=600,
+         engine='django.db.backends.postgresql_psycopg2'
+         )
+        
+    
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
