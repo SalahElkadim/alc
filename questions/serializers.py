118b26b9c5ecf3118634a3a004f78a8c49b0fef7
unknown <khaledgaber494@gmail.com>
2025-08-11 18:10:02 +0300

    errormessage

--- questions/serializers.py
+++ questions/serializers.py
@@ -205,7 +205,8 @@ class ReadingQuestionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ReadingQuestion
-        fields = ['id', 'passage', 'text', 'correct_answer', 'reading_choices','book']
+        fields = ['id', 'passage', 'text', 'correct_answer', 'reading_choices', 'book']
+        read_only_fields = ['passage']
 
     def validate_reading_choices(self, value):
         if value:
