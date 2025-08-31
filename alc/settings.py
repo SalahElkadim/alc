52e89114f09507caac64f7644c1ef5579b11f5a5
unknown <khaledgaber494@gmail.com>
2025-08-31 19:38:31 +0300

    edit

--- alc/settings.py
+++ alc/settings.py
@@ -180,9 +180,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "sm249481@gmail.com"
-EMAIL_HOST_PASSWORD = "lptj jnta uqln rbsw"
-DEFAULT_FROM_EMAIL = "sm249481@gmail.com"
+EMAIL_HOST_USER = "salah.mohamed.elkadim@gmail.com"
+EMAIL_HOST_PASSWORD = "fqzd njeb fffg sact"
+DEFAULT_FROM_EMAIL = "salah.mohamed.elkadim@gmail.com"
 
 
 
