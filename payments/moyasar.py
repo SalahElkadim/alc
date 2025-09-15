076f3139015648abfb81727f2a94a3534a2b4dbd
unknown <khaledgaber494@gmail.com>
2025-09-15 03:36:08 +0300

    payment adding

--- payments/moyasar.py
+++ payments/moyasar.py
@@ -14,7 +14,7 @@ def create_payment(given_id, amount, currency="SAR", description=None, callback_
         "amount": amount,
         "currency": currency,
         "description": description,
-        "callback_url": None,
+        "callback_url": "https://alc-production-8568.up.railway.app/payments/callback/",
         "redirect_url": "https://alc-production-8568.up.railway.app/payments/redirect/",
         "source": source,
         "metadata": metadata,
