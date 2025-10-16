519dfb094faa134d97c97b303032ade0d0e77a1b
unknown <khaledgaber494@gmail.com>
2025-10-16 04:08:06 +0300

    dd

--- alc/settings.py
+++ alc/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY="django-insecure-3#-m1uyln4jei7me&3=*+ued3w403@(72wxzg#$2@o_s@so_7l"
 # SECURITY WARNING: don't run with debug turned on in production!
 
 ALLOWED_HOSTS = [
-    "alc-production-5d34.up.railway.app  " , 
+    "alc-production-5d34.up.railway.app" , 
     "https://alc-production-5d34.up.railway.app",
     "localhost",
     "127.0.0.1",
