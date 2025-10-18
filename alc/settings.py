04015851f9e8d2bca91f39327bd99ee531c3a836
unknown <khaledgaber494@gmail.com>
2025-10-18 14:57:19 +0300

    sss

--- alc/settings.py
+++ alc/settings.py
@@ -104,6 +104,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "https://alc-production-5d34.up.railway.app",
     "http://localhost:3000",   
+    "https://alcreactapp-production.up.railway.app/"
 ]
 AUTH_USER_MODEL = 'users.CustomUser'
 ROOT_URLCONF = 'alc.urls'
