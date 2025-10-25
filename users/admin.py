0d317a9f957fe2f7b8801a2f0d8ffe34696b9595
unknown <khaledgaber494@gmail.com>
2025-10-25 11:45:23 +0300

    sss

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, UserBook
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
@@ -21,6 +21,7 @@ class CustomUserAdmin(UserAdmin):
     )
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(UserBook)
 
 
 
