91bfb659ffd37ebc261a22c18bb91ef59cd0cf75
unknown <khaledgaber494@gmail.com>
2025-10-27 06:56:57 +0300

    sss

--- alc/settings.py
+++ alc/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+#DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
